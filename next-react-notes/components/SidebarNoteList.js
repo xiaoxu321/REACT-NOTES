a1f8f55235ccda9bacb25b36fa11b41c770e0494
bertXu <453630401@qq.com>
2024-05-31 15:15:15 +0800

    feat:侧边栏展开和收缩功能,EditButton组件和Suspense fallback骨架图

--- next-react-notes/components/SidebarNoteList.js
+++ next-react-notes/components/SidebarNoteList.js
@@ -1,7 +1,10 @@
-// 'use client'
-import dayjs from 'dayjs';
+import SidebarNoteItem from '@/components/SidebarNoteItem';
+import { getAllNotes } from '@/lib/redis'
 
-export default async function NoteList({ notes }) {
+export default async function NoteList() {
+    const sleep = ms => new Promise(r => setTimeout(r, ms));
+    await sleep(5000);
+    const notes = await getAllNotes()
 
     const arr = Object.entries(notes);
 
@@ -13,12 +16,8 @@ export default async function NoteList({ notes }) {
 
     return <ul className="notes-list">
         {arr.map(([noteId, note]) => {
-            const { title, updateTime } = JSON.parse(note);
             return <li key={noteId}>
-                <header className="sidebar-note-header">
-                    <strong>{title}</strong>
-                    <small>{dayjs(updateTime).format('YYYY-MM-DD hh:mm:ss')}</small>
-                </header>
+                <SidebarNoteItem noteId={noteId} note={JSON.parse(note)} />
             </li>
         })}
     </ul>
